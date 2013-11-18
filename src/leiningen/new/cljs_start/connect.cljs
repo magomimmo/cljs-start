0075f82009978e10817c24a8ea1e1746ba41f977
magomimmo <mimmo.cosenza@gmail.com>
2013-11-18 22:14:14 +0100

    modify for austin

--- src/leiningen/new/cljs_start/connect.cljs
+++ src/leiningen/new/cljs_start/connect.cljs
@@ -1,7 +1,7 @@
 ;;; This namespace is for creating the connection with the browser. It
-;;; lives in the dev-resources/tools/http directory. It is used in the
+;;; lives in the dev-resources/tools/brepl directory. It is used in the
 ;;; :dev profile only.
 (ns brepl.connect
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl]))
 
-(repl/connect "http://localhost:9000/repl")
+;;(repl/connect "http://localhost:9000/repl")
