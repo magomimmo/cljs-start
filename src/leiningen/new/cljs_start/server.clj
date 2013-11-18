b7b68e412ec1169e0f1d4094ee8d7aa27a4718c1
magomimmo <mimmo.cosenza@gmail.com>
2013-11-18 22:17:48 +0100

    add enlive for supporting austin

--- src/leiningen/new/cljs_start/server.clj
+++ src/leiningen/new/cljs_start/server.clj
@@ -1,22 +1,30 @@
 ;;; This namespace is used for development and testing purpose only.
 (ns ring.server
-  (:require [compojure.core :refer (GET defroutes)]
-            [compojure.route :refer  (resources not-found)]
-            [ring.util.response :refer (redirect)]
-            [ring.adapter.jetty :as jetty]))
+  (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
+            [net.cgrand.enlive-html :as enlive]
+            [compojure.route :refer  (resources)]
+            [compojure.core :refer (GET defroutes)]
+            [ring.adapter.jetty :as jetty]
+            [clojure.java.io :as io]))
+
+;;; We use enlive lib to add to the body of the index.html page the
+;;; script tag containing the JS code which activates the bREPL
+;;; connection.
+(enlive/deftemplate page
+  (io/resource "public/index.html")
+  []
+  [:body] (enlive/append
+            (enlive/html [:script (browser-connected-repl-js)])))
 
 ;;; defroutes macro defines a function that chains individual route
 ;;; functions together. The request map is passed to each function in
 ;;; turn, until a non-nil response is returned.
 (defroutes site
-  ; to serve document root address
-  (GET "/" [] (redirect "/index.html"))
-  ; to serve static pages saved in dev-resources/public directory
   (resources "/")
-  ; if page is not found
-  (not-found "Page not found"))
+  (GET "/*" req (page)))
 
-;;; 
+;;; To run the jetty server. The server symbol is not private to
+;;; allows to start and stop thejetty server from the repl.
 (defn run
   "Run the ring server. It defines the server symbol with defonce."
   []
