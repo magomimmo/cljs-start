e737b21de586ef56c294a699571e3a748a488464
magomimmo <mimmo.cosenza@gmail.com>
2013-12-24 15:31:56 +0100

    rename core_test.cljs to test.cljs

--- src/leiningen/new/cljs_start/test.cljs
+++ src/leiningen/new/cljs_start/test.cljs
@@ -1,9 +1,9 @@
 ;;; This namespace is used for testing purpose. It use the
 ;;; clojurescript.test lib.
-(ns {{namespace}}.core-test
+(ns {{namespace}}-test
   (:require-macros [cemerick.cljs.test :as m :refer (deftest testing are)])
   (:require [cemerick.cljs.test :as t]
-            [{{namespace}}.core :refer (foo)]))
+            [{{namespace}} :refer (foo)]))
 
 (deftest foo-test
   (testing "I don't do a lot\n"
