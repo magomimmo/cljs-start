63d9bf0d0ab05cb9139672babd49a1bf57a55d30
magomimmo <mimmo.cosenza@gmail.com>
2013-12-24 15:32:39 +0100

    update release to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-start/lein-template "0.0.9"
+(defproject cljs-start/lein-template "0.1.0"
   :description "A lein-template for creating ClojureScript libs with batteries included "
   :url "https://github.com/magomimmo/cljs-start"
   :license {:name "Eclipse Public License"
